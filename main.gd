6abf837ace854d08e03f50f805aa7e5dc8e10b59
Rafael <rafaelcrd.ribeiro@gmail.com>
2023-02-05 17:44:42 -0300

    feat: mute songs option added to game scene

--- main.gd
+++ main.gd
@@ -7,3 +7,14 @@ func _on_Ball_fall_pl_1():
 
 func _on_Ball_fall_pl_2():
 	player_position()
+
+var mute
+
+func _on_MuteButton_pressed():
+	mute = !mute
+	if mute == true: 
+		$BackGround.stop()
+		$AudioStreamPlayer.stop()
+	else:
+		$BackGround.play()
+		$AudioStreamPlayer.stop()
